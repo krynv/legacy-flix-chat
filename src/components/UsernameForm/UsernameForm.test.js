efb0acae10db83b443ee434558accbc314990e1a
VK <vitaliy.krynytskyy@gmail.com>
2019-07-25 16:40:27 +0100

    Updated Tests

--- src/components/UsernameForm/UsernameForm.test.js
+++ src/components/UsernameForm/UsernameForm.test.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom';
 import UsernameForm from './UsernameForm';
 import { configure, mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { exportAllDeclaration } from '@babel/types';
+import { stub } from 'sinon';
 
 configure({ adapter: new Adapter() });
 
 describe('<UsernameForm />', () => {
-
-    const onSubmitFn = jest.fn();
-    const onChangeFn = jest.fn();
-
-    const component = shallow(<UsernameForm onSubmit={onSubmitFn} onChange={onChangeFn} />);
-    const form = component.find('input');
 
     it('component renders wihout crashing', () => {
         const div = document.createElement('div');
@@ -21,14 +15,19 @@ describe('<UsernameForm />', () => {
         ReactDOM.unmountComponentAtNode(div);
     });
 
-    // it('onSubmit gets handled correctly', () => {
-    //     form.props().onSubmit({
+    it('onSubmit gets handled correctly', () => {
+        let onSubmitFn = jest.fn();
+        let wrapper = mount(< UsernameForm onSubmit={onSubmitFn} />);
+        let inputForm = wrapper.find('form');
 
-    //     });
-    //     // expect(onSubmitFn.calledOnce).to.equal(true);
-    // });
+        inputForm.simulate('submit');
+        expect(onSubmitFn).toHaveBeenCalledTimes(1);
+    });
 
     it('onChange event is handled correctly', () => {
+        let onChangeFn = jest.fn();
+        let component = shallow(<UsernameForm onChange={onChangeFn} />);
+        let form = component.find('input');
 
         //trigger onChange event of our component
         form.props().onChange({
